258527c55e5fe0ca3f4080264e961b9e4a549f91
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-28 17:43:04 +0530

    [Tushar Vidwans] Adding Welcome Message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LineComparison_Day8
+{
+
+    public class Program
+    {
+
+        /// <summary>
+        /// Welcome Message for this program & used for running other classes related to LineComputation namespace
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Line Comparison Computation Program");
+
+        }
+
+    }
+}
