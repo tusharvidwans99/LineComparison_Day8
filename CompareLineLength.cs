3cffe13b704a334850ec5eaef22dcaa484e5328a
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-28 18:06:06 +0530

    [Tushar Vidwans] Checking whether Two Line are Identical or Not

--- CompareLineLength.cs
+++ CompareLineLength.cs
@@ -8,24 +8,38 @@ namespace LineComparison_Day8
 {
     public class CompareLineLength
     {
-        int x1, y1, x2, y2;
-        public void get()
+        int x1, x2, y1, y2;
+        public CompareLineLength(int a)
         {
-            Console.WriteLine("Enter X start coordinate number of line1 i.e x1: ");
-            x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Y start coordinate number of line1 i.e y1: ");
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter X end coordinate number of line i.e x2: ");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Y end coordinate number of line1 i.e y2: ");
-            y2 = Convert.ToInt32(Console.ReadLine());
-            MesuretheLength(x1, y1, x2, y2);
+            if (a != 0)
+            {
+                Console.WriteLine($"Enter X start coordinate number of line{a} i.e x1: ");
+                x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter Y start coordinate number of line{a} i.e y1: ");
+                y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter X end coordinate number of line{a} i.e x2: ");
+                x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter Y end coordinate number of line{a} i.e y2: ");
+                y2 = Convert.ToInt32(Console.ReadLine());
+            }
+          
+            
         }
-        public static void MesuretheLength(int x1, int y1, int x2, int y2)
+        public void MesuretheLength()
         {
+            CompareLineLength line1 = new CompareLineLength(1);
+            CompareLineLength line2 = new CompareLineLength(2);
+            double lengthofLine = Math.Sqrt(Math.Pow((line1.x2 - line1.x1), 2) + Math.Pow((line1.y2 - line1.y1), 2));
+            double lengthofLine2 = Math.Sqrt(Math.Pow((line2.x2 - line2.x1), 2) + Math.Pow((line2.y2 - line2.y1), 2));
 
-            double lengthofLine = Math.Round(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)),4);
-            Console.WriteLine(lengthofLine);
+            if (lengthofLine2 == lengthofLine)
+            {
+                Console.WriteLine("Both lines are Equal OR Identical");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are non identical");
+            }
         }
 
     }
